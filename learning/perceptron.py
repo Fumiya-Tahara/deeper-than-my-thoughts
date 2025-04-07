de361686706292acfb08f868464bd2711c7aaa7b
Fumiya Tahara <taharafumiya0219@gmail.com>
2025-04-07 22:49:26 +0900

    Add Nand to perceptron

--- learning/perceptron.py
+++ learning/perceptron.py
@@ -5,3 +5,11 @@ def And(x1, x2):
         return 0
     elif tmp > theta:
         return 1
+
+def Nand(x1, x2):
+    w1, w2, theta = -0.5, -0.5, -0.7
+    tmp = x1*w1 + x2*w2
+    if tmp <= theta:
+        return 1
+    elif tmp > theta:
+        return 0
