2f7e47bd6d9b4dec3ec403aed20ecc8541a0186f
Fumiya Tahara <taharafumiya0219@gmail.com>
2025-04-07 22:56:48 +0900

    Add Or to perceptron

--- learning/perceptron.py
+++ learning/perceptron.py
@@ -13,3 +13,11 @@ def Nand(x1, x2):
         return 1
     elif tmp > theta:
         return 0
+
+def Or(x1, x2):
+    w1, w2, theta = 0.5, 0.5, 0.2
+    tmp = x1*w1 + x2*w2
+    if tmp <= theta:
+        return 0
+    elif tmp > theta:
+        return 1
