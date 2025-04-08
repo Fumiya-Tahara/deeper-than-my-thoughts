df0fea2f3b05d50d1ad3a7f3306cbafcacfa949f
Fumiya Tahara <taharafumiya0219@gmail.com>
2025-04-08 10:41:45 +0900

    Fix Nand function

--- learning/perceptron/normal_ver.py
+++ learning/perceptron/normal_ver.py
@@ -9,9 +9,9 @@ def And(x1, x2):
 def Nand(x1, x2):
     w1, w2, theta = -0.5, -0.5, -0.7
     tmp = x1*w1 + x2*w2
-    if tmp <= theta:
+    if tmp > theta:
         return 1
-    elif tmp > theta:
+    elif tmp <= theta:
         return 0
 
 def Or(x1, x2):
